5977b636d32df5f90b7ee4f4414c7d6399778119
AgungSetyaPratama <agungwas@gmail.com>
2021-02-02 15:19:06 +0700

    fixing deploy

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const route = require('express').Router()
 const userRoutes = require('./userRoutes')
 const reviewRoutes = require('./reviewRoutes')
 
+route.get('/', (req, res) => { res.status(200).json("Selamat datang!\r\nAgung Setya Pratama")})
 route.use('/user', userRoutes)
 route.use('/review', reviewRoutes)
 
